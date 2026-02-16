e28b55b2a8873b1a4f93c53614b9dcb5f9b02cde
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2026-02-16 22:18:29 +0100

    add rice_calc

--- src/calc.py
+++ src/calc.py
@@ -10,4 +10,12 @@ def ingers_equation(value1 = None, value2 = None, proc1 = None, proc2 = None):
     if value1 != None and value2 != None and proc1 == None and proc2 == None:
         proc1 = 100
         return (value2 * proc1) / value2
-    return
+    if value1 != None and value2 != None and proc1 != None and proc2 == None:
+        return (value2 * proc1) / value2
+    return None
+
+def water_to_rice(amount):
+    benchmark_rice = 450
+    benchmark_warter = 1000
+    amount_of_water = ingers_equation(benchmark_rice, amount, benchmark_warter)
+    return f"You will need {amount_of_water}ml"
