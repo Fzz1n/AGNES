5be9b4f9fc1493a282d642c1590837117a9f8a69
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2026-02-17 09:16:22 +0100

    add rice_calc to AGNES

--- src/main.py
+++ src/main.py
@@ -69,6 +69,9 @@ def main():
             elif "deactivate microphone" in text:
                 speak("copy that")
                 speak(timer.countdown(converter.get_time(text)))
+            elif "rice" in text:
+                number = converter.get_number(text)
+                speak(f"{calc.water_to_rice(number)}")
             elif "exit" in text:
                 print("Exiting program...")
                 break
