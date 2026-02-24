e65800c8201e5be3784d45ebe075e95f1dd52dd1
Fzz1n <02frederik@gmail.com>
2026-02-24 22:50:26 +0100

    fix return value

--- src/sound_effects.py
+++ src/sound_effects.py
@@ -5,7 +5,7 @@ def adjust_sound(value):
     if value <= 100 and value >= 0:
         pyvolume.custom(percent=value)
         return f"The volume is set to {value}"
-    return f"{value}, is not a valid input"
+    return f"Not a valid input"
 
 def play_mp3(soundname):
     dir = "src/mp3_files/"
