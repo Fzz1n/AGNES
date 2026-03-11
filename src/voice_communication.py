ae0ea7894e28e12da8cbe10595e69538ed35d5f0
Fzz1n <02frederik@gmail.com>
2026-03-11 16:08:17 +0100

    add timer to misunderstanding

--- src/voice_communication.py
+++ src/voice_communication.py
@@ -5,6 +5,7 @@ from gtts import gTTS
 import speech_recognition as sr
 
 import src.global_var
+from src.timer import current_time
 
 def speak(text):
     src.global_var.last_answer = text # Saving the answer
@@ -23,22 +24,23 @@ def speak(text):
     os.remove(filename)
 
 def get_audio(r, source, lang):
-    try:
-        audio = r.listen(source) # addition ", timeout=10, phrase_time_limit=8"
-        said = r.recognize_google(audio, language=lang) # Danish -> da-DK || English(US) -> en-US
-        print("You said:", said.lower())
-        return said.lower()
+    while True:
+        try:
+            audio = r.listen(source, phrase_time_limit = 10) # addition ", timeout=10, phrase_time_limit=8"
+            said = r.recognize_google(audio, language=lang) # Danish -> da-DK || English(US) -> en-US
+            print("You said:", said.lower())
+            return said.lower()
 
-    except sr.WaitTimeoutError:
-        print("No sound recorded")
-        return
-    
-    except sr.UnknownValueError:
-        src.global_var.misunderstanding_counter += 1
-        print("Couldn't understand audio")
-        return
-    
-    except sr.RequestError as e:
-        print("Google API error:", e)
-        speak("Google API error. Try again later")
-        return
+        except sr.WaitTimeoutError:
+            print("No sound recorded")
+            continue
+        
+        except sr.UnknownValueError:
+            src.global_var.misunderstanding_counter += 1
+            print(f"Couldn't understand audio {current_time()}")
+            continue
+        
+        except sr.RequestError as e:
+            print("Google API error:", e)
+            speak("Google API error. Try again later")
+            continue
