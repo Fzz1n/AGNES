4f13e0b9ded59bdc7ea69aa7f2762606d06e0bbb
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2026-03-20 13:14:34 +0100

    crearion of mail sender

---
+++ src/mail.py
@@ -0,0 +1,28 @@
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+load_dotenv()
+
+SMTP_SERVER = os.environ["smtp_server"]
+PORT = os.environ["smtp_port"]
+SENDER_EMAIL = os.environ["sender_email"]
+PASSWORD = os.environ["sender_email_password"]
+RECIVER_EMAIL = os.environ["receiver_email"]
+
+message = MIMEMultipart()
+message['From'] = SENDER_EMAIL
+message['To'] = RECIVER_EMAIL
+message['Subject'] = 'Test Email'
+
+# Creation of mail body
+body = 'This is a test email sent from Python.'
+message.attach(MIMEText(body, 'plain'))
+
+# Send the mail
+server = smtplib.SMTP(SMTP_SERVER, PORT)
+server.starttls()
+server.login(SENDER_EMAIL, PASSWORD)
+server.send_message(message)
+server.quit()
