b79785ae9f030dbc9e733068c800d7102e61547f
Fzz1n <02frederik@gmail.com>
2026-04-28 22:45:46 +0200

    move global const to local

--- src/external_services/iot/plugs.py
+++ src/external_services/iot/plugs.py
@@ -2,10 +2,10 @@ import ast
 from src import global_var
 from src.external_services.iot.bridge.homey import get_status
 devices_data = ast.literal_eval(global_var.get_global_var("iot_devices"))
-PLUG_DATA = devices_data["plug"]
-PLUG_DATA_ID = PLUG_DATA["id"]
 
 def plugs(text):
+	PLUG_DATA = devices_data["plug"]
+	PLUG_DATA_ID = PLUG_DATA["id"]
     
 	if "watt" in text:
 		value = get_status(PLUG_DATA_ID, "measure_power")
