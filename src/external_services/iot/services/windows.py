6855c15959fbe01cd7af541d526f3599cf0f42ef
Fzz1n <02frederik@gmail.com>
2026-06-29 21:21:11 +0200

    update so a message can only be said 3 times before getting muted

--- src/external_services/iot/services/windows.py
+++ src/external_services/iot/services/windows.py
@@ -4,19 +4,28 @@ from src import voice_communication, global_var, timer
 
 def should_window_be_open(room):
 	open_window_buffer = False
+	open_window_counter = 0
 	while True:
 		within_time = "07:00:00" < timer.current_time() < "22:00:00"
 		temp = get_device_current_value(f"thermometer_{room}", "measure_temperature")
 		humidity = get_device_current_value(f"thermometer_{room}", "measure_humidity")
-		
-		if humidity and humidity > 40:
-			window_open = get_device_current_value(f"window magnet_{room}", "alarm_contact")
-			if within_time and not (window_open or weather_vs_indoor_climate(temp)):
-				voice_communication.speak(f"Please open the {room} window")
-			else:
-				open_window_buffer = True
-		elif open_window_buffer and within_time and not weather_vs_indoor_climate(temp):
+		weather_ok = weather_allows_the_window_to_be_open(temp)
+		can_notify = within_time and open_window_counter < 3
+
+		def notify():
 			voice_communication.speak(f"Please open the {room} window")
+
+		if humidity is not None and humidity > 40:
+			is_window_open = get_device_current_value(f"window magnet_{room}", "alarm_contact")
+			if is_window_open:
+				open_window_counter = 0
+			elif weather_ok:
+				if can_notify:
+					notify()
+				else:
+					open_window_buffer = True
+		elif open_window_buffer and weather_ok and can_notify:
+			notify()
 			open_window_buffer = False
 		time.sleep(300)
 		
@@ -34,7 +43,7 @@ def should_window_be_closed(room):
 				window_open = get_device_current_value(F"window magnet_{room}", "alarm_contact")
 				data_exist = temp and humidity
 				
-				if weather_vs_indoor_climate(temp) or (data_exist and (temp < 18 or humidity < 40 and temp < 23)):
+				if not weather_allows_the_window_to_be_open(temp) or (data_exist and (temp < 18 or humidity < 40 and temp < 23)):
 					voice_communication.speak(f"Please close the {room} window")
 				time.sleep(250)
 
@@ -43,15 +52,15 @@ def should_window_be_closed(room):
 		
 		time.sleep(3)
 
-def weather_vs_indoor_climate(indoor_temp):
+def weather_allows_the_window_to_be_open(indoor_temp):
 	if not indoor_temp:
-		return True
+		return False
 	
-	temp = global_var.current_weather.get("temp",0) > indoor_temp
-	wind = global_var.current_weather.get("wind",0) > 25
-	humidity = global_var.current_weather.get("humidity",0) > 80
-	precipitation = global_var.current_weather.get("precipitation",1) > 0
-	rain = global_var.current_weather.get("rain",1) > 0
-	snow = global_var.current_weather.get("snow",1) > 0
+	temp = global_var.current_weather.get("temp",20) < indoor_temp			# Hotter indoor vs outdoor
+	wind = global_var.current_weather.get("wind",5) < 25					# A wind speed below 25 m/s
+	humidity = global_var.current_weather.get("humidity",50) < 80			# Humidity outside below 80%
+	precipitation = global_var.current_weather.get("precipitation",0) == 0	# 0 mm of precipitation
+	rain = global_var.current_weather.get("rain",0) == 0					# 0 mm of rain
+	snow = global_var.current_weather.get("snow",0) == 0					# 0 mm of snow
 	
-	return temp or wind or humidity or precipitation or rain or snow
+	return temp and wind and humidity and precipitation and rain and snow
