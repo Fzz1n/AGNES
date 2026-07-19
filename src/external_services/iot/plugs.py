210ee0bd0bccde8a97bc49428368b9e927f16e9f
Fzz1n <02frederik@gmail.com>
2026-07-19 11:47:37 +0200

    cheng plug data fo its fits new dynamic format

--- src/external_services/iot/plugs.py
+++ src/external_services/iot/plugs.py
@@ -4,9 +4,13 @@ from src.external_services.iot.bridge.homey import get_status
 devices_data = ast.literal_eval(global_var.get_global_var("iot_devices"))
 
 def plugs(text):
-	PLUG_DATA = devices_data["plug"]
-	PLUG_DATA_ID = PLUG_DATA["id"]
-    
 	if "watt" in text:
-		value = get_status(PLUG_DATA_ID, "measure_power")
-		return value if value else 0
+		plug_name = None
+		for key, value in devices_data.items():
+			if "plug" in key:
+				plug_name = key
+			
+		if plug_name is None:
+			return False
+		
+		return get_status(devices_data[plug_name]["id"], "measure_power")
